1509e2dd74f4e3d44b61b6dd8e6d5997fcc4e586
fuzzy_bot <fuzzy_bot@arenaga.me>
2013-03-16 15:21:44 -0400

    Added UDP external server query option

--- TabConfig/src/org/mcsg/double0negative/tabconfig/Pinger.java
+++ TabConfig/src/org/mcsg/double0negative/tabconfig/Pinger.java
@@ -5,48 +5,69 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Date;
 
+import me.rmmccann.query.MCQuery;
+import me.rmmccann.query.QueryResponse;
+
 public class Pinger {
 
+    @SuppressWarnings("deprecation")
+    public static int[] ping(String ip, int port) {
 
-	@SuppressWarnings("deprecation")
-	public static int []  ping(String ip, int port){
+	try {
 
+	    Socket sk = new Socket(ip, port);
 
-		try{
-			Socket sk = new Socket(ip,port);
+	    OutputStream out = sk.getOutputStream();
 
-			OutputStream out = sk.getOutputStream();
+	    out.write(0xFE);
+	    out.flush();
 
-			out.write(0xFE);
-			out.flush();
-			
-			long time = new Date().getTime();
-			
-			//System.out.print("w");
-			DataInputStream in = new DataInputStream(sk.getInputStream());
-			//System.out.print("-in-");
+	    long time = new Date().getTime();
 
-			String s = in.readLine();
-		//	System.out.println("r - "+(new Date().getTime() - time)+"ms");
+	    // System.out.print("w");
+	    DataInputStream in = new DataInputStream(sk.getInputStream());
+	    // System.out.print("-in-");
 
-			String s1 = "";
-			for(int a = 0; a<s.length();a+=2){
-				s1 = s1 + s.charAt(a);
-			}
-			String[] s2 = s1.split("ง");
+	    String s = in.readLine();
+	    // System.out.println("r - "+(new Date().getTime() - time)+"ms");
 
-			sk.close();
-			//System.out.println(s2.toString() + s2.length);
-			return new int [] {Integer.parseInt(s2[s2.length-2]), Integer.parseInt(s2[s2.length-1])};
+	    String s1 = "";
+	    for (int a = 0; a < s.length(); a += 2) {
+		s1 = s1 + s.charAt(a);
+	    }
+	    String[] s2 = s1.split("ยง");
 
-		}
-		catch(Exception e){
-			
-			//e.printStackTrace();
-			return new int []{-1,-1};
-		}
-		finally{
-			
-		}
+	    sk.close();
+	    // System.out.println(s2.toString() + s2.length);
+	    return new int[] { Integer.parseInt(s2[s2.length - 2]),
+		    Integer.parseInt(s2[s2.length - 1]) };
+
+	} catch (Exception e) {
+
+	    // e.printStackTrace();
+	    return new int[] { -1, -1 };
+	} finally {
+
 	}
+    }
+
+    public static int[] udpping(String ip, int port) {
+
+	try {
+
+	    MCQuery mcQuery = new MCQuery(ip, port);
+	    QueryResponse response = mcQuery.basicStat();
+
+	    return new int[] { response.getOnlinePlayers(),
+		    response.getMaxPlayers() };
+
+	} catch (Exception e) {
+
+	    // e.printStackTrace();
+	    return new int[] { -1, -1 };
+	} finally {
+
+	}
+
+    }
 }
