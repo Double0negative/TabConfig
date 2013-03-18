14df29b555ea6834987fca0087f101b69486abaf
Troy Frew <troy@crudgegames.com>
2013-03-18 19:21:51 -0400

    pulled in config world changes

--- TabConfig/src/org/mcsg/double0negative/tabconfig/TabConfig.java
+++ TabConfig/src/org/mcsg/double0negative/tabconfig/TabConfig.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -21,287 +22,318 @@ import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitTask;
 import org.mcsg.double0negative.tabapi.TabAPI;
 
-public class TabConfig extends JavaPlugin implements Listener, CommandExecutor {
+public class TabConfig extends JavaPlugin implements Listener, CommandExecutor{
 
-    String[][] tab;
 
-    private boolean updateAllOnPlayerLogin = false;;
-    private boolean updateAllOnPlayerLogout = false;
-    private int updateTimer = -1;
-    private int updateTimerTask = 0;
-    private BukkitTask pingTimerTask;
 
-    private final HashMap<String, int[]> ping = new HashMap<String, int[]>();
+	String[][] tab;
 
-    private void fillPlayers(Player p, int a, int b) {
-	for (Player pl : Bukkit.getOnlinePlayers()) {
-	    TabAPI.setTabString(this, pl, a, b, p.getName()
-		    + ((p.getName().length() < 15) ? TabAPI.nextNull() : ""));
+	private boolean updateAllOnPlayerLogin = false;;
+	private boolean updateAllOnPlayerLogout = false;
+	private int updateTimer = -1;
+        private int updateTimerTask = 0;
+        private BukkitTask pingTimerTask;	
+	private HashMap<String, int[]>ping = new HashMap<String, int[]>();
 
-	    b++;
-	    if (b == 3) {
-		b = 0;
-		a++;
-	    }
-	    if (a > TabAPI.getVertSize()) {
-		return;
-	    }
+        public void onDisable() {
 
-	}
+                if (updateTimerTask > 0) {
+                         Bukkit.getScheduler().cancelTask(updateTimerTask);
+                }
 
-    }
+                pingTimerTask.cancel();
 
-    public void load() {
+        }
+  
+	public void onEnable(){
 
-	this.reloadConfig();
-	FileConfiguration f = this.getConfig();
+		File y = this.getDataFolder();
+		File f = new File(this.getDataFolder(), "config.yml");
+		if(!f.exists()){y.mkdirs(); loadFile("config.yml");}
+		
+		
+		if(Bukkit.getPluginManager().getPlugin("TabAPI") == null){
 
-	updateAllOnPlayerLogin = f.getBoolean("updateAllOnPlayerLogin");
-	updateAllOnPlayerLogout = f.getBoolean("updateAllOnPlayerLogout");
-	updateTimer = f.getInt("updateTimer");
+			System.out.println(ChatColor.DARK_RED + "[TabConfig] TabAPI not detected! -  disabling");
+			return;
+		}		
 
-	for (int a = 0; a < TabAPI.getVertSize(); a++) {
-	    int b = 0;
-	    for (String s : f.getStringList("tab." + (a + 1))) {
-		// System.out.println(a+" "+b+" "+s);
-		int c = 0;
-		while ((c = s.indexOf("{ping", c)) != -1) {
-		    int v = s.indexOf("}", c);
-		    String t = s.substring(c, v);
-		    String[] spl = t.split("!");
-		    ping.put(spl[1], new int[2]);
+		tab = new String[TabAPI.getVertSize()][TabAPI.getHorizSize()];
 
-		    c = v;
 
-		}
 
-		tab[a][b] = s;
-		b++;
+		this.getCommand("tabconfig").setExecutor(this);
 
-		if (b > 2) {
-		    break;
-		}
-	    }
-	}
-
-	if (ping.size() > 0) {
-	    pingTimerTask = Bukkit.getScheduler().runTaskTimerAsynchronously(
-		    this, new Runnable() {
-			public void run() {
-			    for (String s : ping.keySet()) {
-				String full = (s.contains(":")) ? s : s
-					+ ":25565";
-				String[] ip = full.split(":");
-
-				ping.put(
-					s,
-					Pinger.ping(ip[0],
-						Integer.parseInt(ip[1])));
-
-			    }
-			}
-		    }, 100, 100);
-	}
-
-	updateAll();
-    }
-
-    public void loadFile(String file) {
-	File t = new File(this.getDataFolder(), file);
-	System.out.println("Writing new file: " + t.getAbsolutePath());
-
-	try {
-	    t.createNewFile();
-	    FileWriter out = new FileWriter(t);
-	    System.out.println(file);
-	    InputStream is = getClass().getResourceAsStream(file);
-	    InputStreamReader isr = new InputStreamReader(is);
-	    BufferedReader br = new BufferedReader(isr);
-	    String line;
-	    while ((line = br.readLine()) != null) {
-		out.write(line + "\n");
-		System.out.println(line);
-	    }
-	    out.flush();
-	    is.close();
-	    isr.close();
-	    br.close();
-	    out.close();
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
-
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd1,
-	    String commandLabel, String[] args) {
-	PluginDescriptionFile pdfFile = this.getDescription();
-
-	Player player = null;
-	if (sender instanceof Player) {
-	    player = (Player) sender;
-	    if (player.hasPermission("tabapi.reload")) {
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+		
+		
 		load();
-		updateAll();
-		player.sendMessage(ChatColor.GOLD + "TabConfig reloaded!");
-		player.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD
-			+ "TabConfig - Double0negative" + ChatColor.RESET
-			+ ChatColor.RED + " Version: " + pdfFile.getVersion());
-	    } else {
-		player.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD
-			+ "TabAPI - Double0negative" + ChatColor.RESET
-			+ ChatColor.RED + " Version: " + pdfFile.getVersion());
 
-	    }
-	} else {
-	    sender.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD
-		    + "TabAPI - Double0negative" + ChatColor.RESET
-		    + ChatColor.RED + " Version: " + pdfFile.getVersion());
-	    return true;
+
+		System.out.println("[TabConfig] Loaded!");
+
+		if(updateTimer != -1){
+		updateTimerTask = Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){
+				public void run(){
+					updateAll();
+				}
+			}, 10, updateTimer);
+		}
+
 	}
+	
+	public void load(){
+		
+		this.reloadConfig();
+		FileConfiguration f = this.getConfig();
 
-	return true;
 
-    }
+		updateAllOnPlayerLogin = f.getBoolean("updateAllOnPlayerLogin");
+		updateAllOnPlayerLogout = f.getBoolean("updateAllOnPlayerLogout");
+		updateTimer = f.getInt("updateTimer");
+		
+		for(int a = 0; a < TabAPI.getVertSize(); a++){
+			int b = 0; 
+			for(String s: f.getStringList("tab."+(a+1))){
+				//System.out.println(a+" "+b+" "+s);
+				int c = 0;
+				while((c=s.indexOf("{ping",c)) != -1){
+					int v = s.indexOf("}", c);
+					String t = s.substring(c, v);
+					String[] spl = t.split("!");
+					ping.put(spl[1], new int[2]);
+					
+					c = v;
+					
+				}
+				tab[a][b] = s;
+				b++;
 
-    public void onDisable() {
-
-	if (updateTimerTask > 0) {
-	    Bukkit.getScheduler().cancelTask(updateTimerTask);
-	}
-
-	pingTimerTask.cancel();
-
-    }
-
-    public void onEnable() {
-
-	File y = this.getDataFolder();
-	File f = new File(this.getDataFolder(), "config.yml");
-	if (!f.exists()) {
-	    y.mkdirs();
-	    loadFile("config.yml");
-	}
-
-	if (Bukkit.getPluginManager().getPlugin("TabAPI") == null) {
-
-	    System.out.println(ChatColor.DARK_RED
-		    + "[TabConfig] TabAPI not detected! -  disabling");
-	    return;
-	}
-
-	tab = new String[TabAPI.getVertSize()][TabAPI.getHorizSize()];
-
-	this.getCommand("tabconfig").setExecutor(this);
-
-	Bukkit.getServer().getPluginManager().registerEvents(this, this);
-
-	load();
-
-	System.out.println("[TabConfig] Loaded!");
-
-	if (updateTimer != -1) {
-	    updateTimerTask = Bukkit.getScheduler().scheduleSyncRepeatingTask(
-		    this, new Runnable() {
-			public void run() {
-			    updateAll();
+				if(b > 2) break;
 			}
-		    }, 10, updateTimer);
-	}
-
-    }
-
-    @EventHandler
-    public void playerLogin(PlayerLoginEvent e) {
-	final Player p = e.getPlayer();
-	TabAPI.setPriority(this, p, 0);
-	Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-	    public void run() {
-		if (updateAllOnPlayerLogin) {
-		    updateAll();
-		} else {
-		    update(p.getPlayer());
 		}
-	    }
-	}, 2);
-
-    }
-
-    @EventHandler
-    public void playerLogout(PlayerQuitEvent e) {
-	if (updateAllOnPlayerLogout) {
-	    updateAll();
+		
+		
+		if(ping.size() > 0){
+		pingTimerTask =	Bukkit.getScheduler().runTaskTimerAsynchronously(this, new Runnable(){
+				public void run(){
+					for(String s: ping.keySet()){
+						String full = (s.contains(":"))?s:s+":25565";
+						String[] ip = full.split(":");
+						
+						ping.put(s, Pinger.ping(ip[0],Integer.parseInt(ip[1])));
+						
+						
+					}
+				}
+			}, 100, 100);
+		}
+		
+		updateAll();
 	}
 
+	
+	
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd1, String commandLabel, String[] args){
+        PluginDescriptionFile pdfFile = this.getDescription();
+
+        Player player = null;
+        if (sender instanceof Player) {
+            player = (Player) sender;
+            if(player.hasPermission("tabapi.reload")){
+            	load();
+            	updateAll();
+            	player.sendMessage(ChatColor.GOLD+"TabConfig reloaded!");
+                player.sendMessage(ChatColor.DARK_RED +""+ ChatColor.BOLD +"TabConfig - Double0negative"+ChatColor.RESET+  ChatColor.RED +" Version: "+ pdfFile.getVersion() );
+            }
+            else{
+                player.sendMessage(ChatColor.DARK_RED +""+ ChatColor.BOLD +"TabAPI - Double0negative"+ChatColor.RESET+  ChatColor.RED +" Version: "+ pdfFile.getVersion() );
+
+            }
+        }
+        else{
+            sender.sendMessage(ChatColor.DARK_RED +""+ ChatColor.BOLD +"TabAPI - Double0negative"+ChatColor.RESET+  ChatColor.RED +" Version: "+ pdfFile.getVersion() );
+            return true;
+        }
+        
+        return true;
+        
     }
 
-    public String replaceVars(String s, Player p) {
-	String r = s;
+	public String replaceVars(String s, Player p){
+	    try {
+		String r = s;
 
-	r = r.replace("{online}", Bukkit.getOnlinePlayers().length + "");
-	r = r.replace("{max}", Bukkit.getMaxPlayers() + "");
-	r = r.replace("{player}", p.getName());
-	r = r.replace("{displayname}", p.getDisplayName());
-	r = r.replace("{tabname}", p.getPlayerListName());
-	r = r.replace("{servername}", Bukkit.getServerName());
+		r = r.replace("{online}", Bukkit.getOnlinePlayers().length+"");
+		r = r.replace("{max}", Bukkit.getMaxPlayers()+"");
+		r = r.replace("{player}", p.getName());
+		r = r.replace("{displayname}", p.getDisplayName());
+		r = r.replace("{tabname}", p.getPlayerListName());
+		r = r.replace("{servername}", Bukkit.getServerName());
 
-	r = ChatColor.translateAlternateColorCodes('&', r);
+		r = ChatColor.translateAlternateColorCodes('&', r);
 
-	int c = 0;
-	while ((c = r.indexOf("{ping", c)) != -1) {
-	    int v = r.indexOf("}", c);
-	    String t = r.substring(c, v);
-	    String[] spl = t.split("!");
-	    String sy = "";
-	    if (spl[2].equalsIgnoreCase("online")) {
-		sy = ping.get(spl[1])[0] + "";
-	    }
-	    if (spl[2].equalsIgnoreCase("max")) {
-		sy = ping.get(spl[1])[1] + "";
-	    }
-	    // System.out.println(c+"  "+v + "   "+r.length() + "  "+r);
-	    r = r.substring(0, c) + sy + r.substring(v + 1);
+		
+		int c = 0;
+		while((c=r.indexOf("{ping",c)) != -1){
+			int v = r.indexOf("}", c);
+			String t = r.substring(c, v);
+			String[] spl = t.split("!");
+			String sy = "";
+			if(spl[2].equalsIgnoreCase("online")){
+				sy = ping.get(spl[1])[0] +"";
+			}
+			if(spl[2].equalsIgnoreCase("max")){
+				sy = ping.get(spl[1])[1] +"";
+			}
+			//System.out.println(c+"  "+v + "   "+r.length() + "  "+r);
+			r = r.substring(0,c) 
+					+ sy 
+					+ r.substring(v+1);
 
-	    c = c + sy.length();
-
-	}
-
-	return r;
-
-    }
-
-    public void update(Player p) {
-
-	for (int a = 0; a < tab.length; a++) {
-	    for (int b = 0; b < tab[a].length; b++) {
-
-		// System.out.println(a + " "+b);
-		if (tab[a][b] != null) {
-		    String y = replaceVars(tab[a][b], p);
-
-		    if (y.equalsIgnoreCase("{fillplayers}")) {
-			fillPlayers(p, a, b);
-			break;
+			
+			c = c + sy.length();
+			
+			
+		}
+		
+		int d = 0;
+		while((d=r.indexOf("{worldonline",d)) != -1) {
+		    int v = r.indexOf("}", d);
+		    String t = r.substring(d, v);
+		    String[] spl = t.split("!");
+		    String sy = "";
+		       
+		    for (World world : Bukkit.getWorlds()) {
+			
+			if (world.getName().equalsIgnoreCase(spl[1])) {
+			    sy = String.valueOf(Bukkit.getWorld(spl[1]).getPlayers().size());
+			}
+			
 		    }
-
-		    TabAPI.setTabString(this, p, a, b, y
-			    + ((y.length() < 15) ? TabAPI.nextNull() : ""));
+		    
+		    if (sy.equals("")) {
+			sy = "-1";
+		    }
+		    
+		    r = r.substring(0,d) + sy + r.substring(v+1);
+		    
+		    d = d + sy.length();
+		    
 		}
+		
+		return r;
+	    } catch (Exception e) {
+		e.printStackTrace();
+		return null;
 	    }
 	}
 
-	TabAPI.updatePlayer(p);
-
-    }
-
-    public void updateAll() {
-	for (Player p : Bukkit.getOnlinePlayers()) {
-	    update(p);
+	public void updateAll(){
+		for(Player p: Bukkit.getOnlinePlayers()){
+			update(p);
+		}
 	}
-    }
+
+	public void update(Player p){
+
+		for(int a = 0; a < tab.length; a++){
+			for(int b = 0 ;b < tab[a].length;b++){
+
+				//System.out.println(a + " "+b);
+				if(tab[a][b] != null){
+					String y = replaceVars(tab[a][b], p);
+
+					if(y.equalsIgnoreCase("{fillplayers}")){ 
+						fillPlayers(p, a, b);
+						break;
+					}
+
+
+					TabAPI.setTabString(this, p, a, b, y + ((y.length() < 15) ? TabAPI.nextNull(): "" ));
+				}
+			}
+		}
+
+		TabAPI.updatePlayer(p);
+
+
+	}
+
+
+	private void fillPlayers(Player p, int a, int b) {
+		for(Player pl:Bukkit.getOnlinePlayers()){
+			TabAPI.setTabString(this, pl, a, b, p.getName()+ ((p.getName().length() < 15) ? TabAPI.nextNull(): ""));
+
+			b++;
+			if(b == 3){
+				b = 0; 
+				a++;
+			}
+			if(a > TabAPI.getVertSize()){
+				return;
+			}
+
+		}
+
+	}
+
+	@EventHandler
+	public void playerLogin(PlayerLoginEvent e){
+		final Player p = e.getPlayer();
+		TabAPI.setPriority(this, p, 0);
+		Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable(){
+			public void run(){
+				if(updateAllOnPlayerLogin){
+					updateAll();
+				}
+				else{
+					update(p.getPlayer());
+				}
+			}
+		}, 2);
+
+
+	}
+
+	@EventHandler
+	public void playerLogout(PlayerQuitEvent e){
+		if(updateAllOnPlayerLogout){
+			updateAll();
+		}
+
+	}
+	
+	
+	public void loadFile(String file){
+		File t = new File(this.getDataFolder(), file);
+		System.out.println("Writing new file: "+ t.getAbsolutePath());
+			
+			try {
+				t.createNewFile();
+				FileWriter out = new FileWriter(t);
+				System.out.println(file);
+				InputStream is = getClass().getResourceAsStream(file);
+				InputStreamReader isr = new InputStreamReader(is);
+				BufferedReader br = new BufferedReader(isr);
+				String line;
+				while ((line = br.readLine()) != null) {
+					out.write(line+"\n");
+					System.out.println(line);
+				}
+				out.flush();
+				is.close();
+				isr.close();
+				br.close();
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		
+	}
+
 
 }
